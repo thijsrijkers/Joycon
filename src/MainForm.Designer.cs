0b944b8044b9badb227a2f11c796ad9d5fc578a9
thijsrijkers <thijsrijkers@live.nl>
2020-11-08 16:03:10 +0100

    +Project setup based on Joycon4CS

---
+++ src/MainForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace JoyConForm
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.buttonScan = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timerUpdate = new System.Windows.Forms.Timer(this.components);
+            this.picture1 = new System.Windows.Forms.PictureBox();
+            this.picture2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonScan
+            // 
+            this.buttonScan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonScan.ForeColor = System.Drawing.Color.White;
+            this.buttonScan.Location = new System.Drawing.Point(47, 12);
+            this.buttonScan.Name = "buttonScan";
+            this.buttonScan.Size = new System.Drawing.Size(90, 35);
+            this.buttonScan.TabIndex = 0;
+            this.buttonScan.Text = "Scan";
+            this.buttonScan.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.buttonScan.UseVisualStyleBackColor = false;
+            this.buttonScan.Click += new System.EventHandler(this.buttonScan_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonStart.ForeColor = System.Drawing.Color.White;
+            this.buttonStart.Location = new System.Drawing.Point(47, 53);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(90, 23);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Start polling";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // timerUpdate
+            // 
+            this.timerUpdate.Interval = 16;
+            this.timerUpdate.Tick += new System.EventHandler(this.timerUpdate_Tick);
+            // 
+            // picture1
+            // 
+            this.picture1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picture1.Location = new System.Drawing.Point(47, 82);
+            this.picture1.Name = "picture1";
+            this.picture1.Size = new System.Drawing.Size(275, 275);
+            this.picture1.TabIndex = 7;
+            this.picture1.TabStop = false;
+            // 
+            // picture2
+            // 
+            this.picture2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picture2.Location = new System.Drawing.Point(439, 82);
+            this.picture2.Name = "picture2";
+            this.picture2.Size = new System.Drawing.Size(275, 275);
+            this.picture2.TabIndex = 8;
+            this.picture2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(717, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 52);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "X";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(780, 423);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picture2);
+            this.Controls.Add(this.picture1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonScan);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "JoyCon";
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).EndInit();
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonScan;
+		private System.Windows.Forms.Button buttonStart;
+		private System.Windows.Forms.Timer timerUpdate;
+		private System.Windows.Forms.PictureBox picture1;
+		private System.Windows.Forms.PictureBox picture2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
