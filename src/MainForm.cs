cc236de4ff42f6144d04fca5efe07309796d5c46
thijsrijkers <thijsrijkers@live.nl>
2020-11-08 17:26:12 +0100

    +tested the motion controlls +- cleaned up the UI

--- src/MainForm.cs
+++ src/MainForm.cs
@@ -12,17 +12,14 @@ namespace JoyConForm
 			InitializeComponent();
 
 			drawOrigin = new Point(picture1.Width / 2, picture1.Height / 2);
-			leftCon = new Math3D.Math3D.Cube(200, 100, 20);
-			rightCon = new Math3D.Math3D.Cube(200, 100, 20);
+			JoyCon1 = new Math3D.Math3D.Cube(200, 100, 20);
 
 		}
 
 		JoyconManager joyconManager = new JoyconManager();
 
 		Point drawOrigin;
-		Math3D.Math3D.Cube leftCon;
-		Math3D.Math3D.Cube rightCon;
-
+		Math3D.Math3D.Cube JoyCon1;
 
 		private void buttonScan_Click(object sender, EventArgs e)
 		{
@@ -59,33 +56,13 @@ namespace JoyConForm
 			{
 				var j = joyconManager.j[0];
 
-				leftCon.InitializeCube();
-				leftCon.RotateX = (float)(j.GetVector().eulerAngles.Y * 180.0f / Math.PI);
-				leftCon.RotateY = (float)(j.GetVector().eulerAngles.Z * 180.0f / Math.PI);
-				leftCon.RotateZ = (float)(j.GetVector().eulerAngles.X * 180.0f / Math.PI);
+				JoyCon1.InitializeCube();
+				JoyCon1.RotateX = (float)(j.GetVector().eulerAngles.Y * 180.0f / Math.PI);
+				JoyCon1.RotateY = (float)(j.GetVector().eulerAngles.Z * 180.0f / Math.PI);
+				JoyCon1.RotateZ = (float)(j.GetVector().eulerAngles.X * 180.0f / Math.PI);
 
-				picture1.Image = leftCon.DrawCube(drawOrigin);
+				picture1.Image = JoyCon1.DrawCube(drawOrigin);
 			}
-
-			if (joyconManager.j.Count > 1)
-			{
-				var j = joyconManager.j[1];
-
-				picture2.Image = rightCon.DrawCube(drawOrigin);
-
-				rightCon.InitializeCube();
-				rightCon.RotateX = (float)(j.GetVector().eulerAngles.Y * 180.0f / Math.PI);
-				rightCon.RotateY = (float)(j.GetVector().eulerAngles.Z * 180.0f / Math.PI);
-				rightCon.RotateZ = (float)(j.GetVector().eulerAngles.X * 180.0f / Math.PI);
-
-				picture1.Image = rightCon.DrawCube(drawOrigin);
-			}
-		}
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-			joyconManager.OnApplicationQuit();
-			System.Windows.Forms.Application.Exit();
 		}
     }
 }
